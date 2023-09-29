f181458496e7a26875517a6053ed34ab1e8fd63d
Yuriy <yd.appdeveloper@gmail.com>
2023-09-29 16:15:11 -0600

    Deleting array row State

--- tasktree/src/App.js
+++ tasktree/src/App.js
@@ -9,6 +9,10 @@ function App() {
     { id: 7546, name: "Watch Lectures", completed: false }
   ]);
 
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
   return (
     <div className="App">
       <h1>Task list</h1>
@@ -16,7 +20,7 @@ function App() {
         {tasks.map((task) => (
           <li key={task.id}>
             <span>{task.id} - {task.name}</span>
-            <button className='delete'>Delete</button>
+            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
           </li>
 
         ))}
