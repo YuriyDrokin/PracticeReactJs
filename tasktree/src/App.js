79ade13d93ba1bf23ff76f0ac225c73a9a32c7d6
Yuriy <yd.appdeveloper@gmail.com>
2023-09-30 16:38:00 -0600

    Multiple Components and Fragments

--- tasktree/src/App.js
+++ tasktree/src/App.js
@@ -1,35 +1,16 @@
-import { useState } from 'react';
+
+import { Header } from './components/Header';
 import './App.css';
+import { TaskList } from './components/TaskList';
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    { id: 5432, name: "Record React Lectures", completed: true },
-    { id: 8562, name: "Edit React Lectures", completed: false },
-    { id: 7546, name: "Watch Lectures", completed: false }
-  ]);
 
-  const [show, setShow] = useState(true);
-
-  function handleDelete(id) {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
 
   return (
     <div className="App">
-      <h1>Task list</h1>
-      <ul>
-        <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
-        {show && tasks.map((task) => (
-
-
-          <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-            <span>{task.id} - {task.name}</span>
-            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-          </li>
-
-        ))}
-      </ul>
+      <Header />
+      <TaskList title="Random" subtitle="Test" />
     </div>
   );
 }
---
+++ tasktree/src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from "../assets/FullLogo_Transparent_NoBuffer.png"
+
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="" />
+      <a href="/">Home</a>
+    </header>
+  )
+}
