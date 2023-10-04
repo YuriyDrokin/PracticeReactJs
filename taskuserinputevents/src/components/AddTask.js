d7958d02750253bd37008784a94475ae073d1cd8
Yuriy <yd.appdeveloper@gmail.com>
2023-10-04 12:13:10 -0600

    Controlled inputs

--- taskuserinputevents/src/components/AddTask.js
+++ taskuserinputevents/src/components/AddTask.js
@@ -9,14 +9,18 @@ export const AddTask = () => {
     setTaskValue(event.target.value);
   }
 
+  const handleReset = () => {
+    setTaskValue("");
+  }
   return (
     <section className="addtask">
       <form>
 
-        <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
+        <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} />
         <button type="submit" >Add Task</button>
+        <span onClick={handleReset} className="reset">Reset</span>
       </form>
-      <p>{taskValue.length}</p>
+      <p>{taskValue}</p>
     </section>
   )
 }
