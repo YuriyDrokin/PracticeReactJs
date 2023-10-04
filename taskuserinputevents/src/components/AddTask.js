dac92cdcba05e0777eae50fc7e93e02cb7fc6348
Yuriy <yd.appdeveloper@gmail.com>
2023-10-04 11:57:10 -0600

    onChangeEvent

--- taskuserinputevents/src/components/AddTask.js
+++ taskuserinputevents/src/components/AddTask.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./AddTask.css";
 
 export const AddTask = () => {
+
+  const [taskValue, setTaskValue] = useState("");
+
+  const handleChange = (event) => {
+    setTaskValue(event.target.value);
+  }
+
   return (
     <section className="addtask">
       <form>
-        <label htmlFor="task">Task Name:</label>
-        <input type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
+
+        <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
         <button type="submit" >Add Task</button>
       </form>
+      <p>{taskValue.length}</p>
     </section>
   )
 }
