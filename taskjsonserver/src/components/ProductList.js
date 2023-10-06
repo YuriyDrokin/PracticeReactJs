468f3e56ae8488595045ce16145a17fef04c313a
Yuriy <yd.appdeveloper@gmail.com>
2023-10-06 17:42:08 -0600

    List of Output

--- taskjsonserver/src/components/ProductList.js
+++ taskjsonserver/src/components/ProductList.js
@@ -15,6 +15,18 @@ export const ProductList = () => {
 
 
   return (
-    <div>ProductList</div>
+    <section>
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <p className="id">{product.id}</p>
+          <p className="name">{product.name}</p>
+          <p className="info">
+            <span>${product.price}</span>
+            <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+          </p>
+        </div>
+      ))
+      }
+    </section >
   )
 }
