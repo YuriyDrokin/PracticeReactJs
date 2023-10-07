658f566c0a6cf631118da27ca8d1781adb2b4eb8
Yuriy <yd.appdeveloper@gmail.com>
2023-10-06 21:26:37 -0600

    created custom Hook

--- taskjsonserver/src/components/ProductList.js
+++ taskjsonserver/src/components/ProductList.js
@@ -1,27 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
+import { useFetch } from "./hooks/useFetch";
 
 export const ProductList = () => {
 
-  const [products, setProducts] = useState([]);
-
-  const [url, setUrl] = useState("http://localhost:8000/products")
 
 
-  const fetchProducts = useCallback(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-  }, [url]);
-
-
-
-  useEffect(() => {
-
-    fetchProducts();
-    console.log("-")
-  }, []);
-
-
+  const [url, setUrl] = useState("http://localhost:8000/products");
+  const { data: products } = useFetch(url);
 
 
   return (
@@ -32,7 +17,7 @@ export const ProductList = () => {
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
       </div>
 
-      {products.map((product) => (
+      {products && products.map((product) => (
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>
           <p className="name">{product.name}</p>
